4c69f576b0b0ed95eeb5eca1a2f5272b24c4ddf0
AaronMcHatton <40744113+AaronMcHatton@users.noreply.github.com>
2023-07-21 16:45:46 -0500

    updating to AddScoped

--- BreakfastTutorial/BreakfastTutorial/Services/Breakfasts/BreakfastService.cs
+++ BreakfastTutorial/BreakfastTutorial/Services/Breakfasts/BreakfastService.cs
@@ -4,7 +4,7 @@ using BreakfastTutorial.Services.Breakfasts;
 
 public class BreakfastService : IBreakfastService
 {
-    private readonly Dictionary<Guid, Breakfast> _breakfasts = new();
+    private static readonly Dictionary<Guid, Breakfast> _breakfasts = new();
     public void CreateBreakfast(Breakfast breakfast)
     {
         _breakfasts.Add(breakfast.id, breakfast);
